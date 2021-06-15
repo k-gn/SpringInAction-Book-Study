28c8d7724b7521439885370e27e038a0f6badd3d
k-gn <rlarbska97@gmail.com>
2021-06-15 15:56:12 +0900

    Taco 프로젝트 진행중

--- taco-cloud/src/main/java/tacos/web/DesignTacoController.java
+++ taco-cloud/src/main/java/tacos/web/DesignTacoController.java
@@ -7,6 +7,7 @@ import java.util.stream.Collectors;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import lombok.extern.slf4j.Slf4j;
@@ -21,6 +22,7 @@ public class DesignTacoController {
 
 	@GetMapping
 	public String showDesignForm(Model model) {
+		// 식재료 리스트 생성
 		List<Ingredient> ingredients = Arrays.asList(
 				new Ingredient("FLTO", "Flour Tortilla", Type.WRAP),
 				new Ingredient("COTO", "Corn Tortilla", Type.WRAP), 
@@ -33,6 +35,7 @@ public class DesignTacoController {
 				new Ingredient("SLSA", "Salsa", Type.SAUCE),
 				new Ingredient("SRCR", "Sour Cream", Type.SAUCE));
 
+		// 각 타입값을 배열로 생성 (WRAP, PROTEIN , ...)
 		Type[] types = Ingredient.Type.values();
 		for (Type type : types) {
 			log.info("=======================");
@@ -47,7 +50,14 @@ public class DesignTacoController {
 		return "design";
 	}
 
+	// 타입별 식재료를 리스트로 반환하는 메소드
 	private List<Ingredient> filterByType(List<Ingredient> ingredients, Type type) {
 		return ingredients.stream().filter(x -> x.getType().equals(type)).collect(Collectors.toList());
+	}
+	
+	@PostMapping
+	public String processDesign(Taco design) {
+		log.info("processDesign : " + design);
+		return "redirect:/orders/current";
 	}
 }
