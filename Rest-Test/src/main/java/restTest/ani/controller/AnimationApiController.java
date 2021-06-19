78902472739104d3c78e5a9a433a75cc3be1d082
k-gn <rlarbska97@gmail.com>
2021-06-19 22:33:56 +0900

    no message

--- Rest-Test/src/main/java/restTest/ani/controller/AnimationApiController.java
+++ Rest-Test/src/main/java/restTest/ani/controller/AnimationApiController.java
@@ -1,9 +1,16 @@
 package restTest.ani.controller;
 
+import java.util.List;
+
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import lombok.RequiredArgsConstructor;
+import restTest.ani.entity.Animation;
 import restTest.ani.repository.AnimationRepository;
 
 @RestController
@@ -13,5 +20,33 @@ public class AnimationApiController {
 	
 	private final AnimationRepository aniRepo;
 	
+	@GetMapping("/")
+	public List<Animation> aniAll() {
+		
+		return null;
+	}
 	
+	@GetMapping("/{id}")
+	public Animation getAni() {
+		
+		return null;
+	}
+	
+	@PostMapping("/")
+	public String insertAni() {
+		
+		return null;
+	}
+	
+	@PutMapping("/{id}")
+	public String updateAni() {
+		
+		return null;
+	}
+	
+	@DeleteMapping("/{id}")
+	public String deleteAni() {
+		
+		return null;
+	}
 }
