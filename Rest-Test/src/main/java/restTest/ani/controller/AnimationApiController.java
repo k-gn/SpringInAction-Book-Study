db3ba5fa17859ee93d71868f4fe8e0e332679fd7
k-gn <rlarbska97@gmail.com>
2021-06-20 12:35:38 +0900

    no message

--- Rest-Test/src/main/java/restTest/ani/controller/AnimationApiController.java
+++ Rest-Test/src/main/java/restTest/ani/controller/AnimationApiController.java
@@ -4,8 +4,10 @@ import java.util.List;
 
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -22,31 +24,31 @@ public class AnimationApiController {
 	
 	@GetMapping("/")
 	public List<Animation> allAni() {
-		
+		aniRepo.findAll();
 		return null;
 	}
 	
 	@GetMapping("/{id}")
-	public Animation getAni() {
-		
+	public Animation getAni(@PathVariable Long id) {
+		aniRepo.findById(id);
 		return null;
 	}
 	
 	@PostMapping("/")
-	public String insertAni() {
-		
+	public String insertAni(@RequestBody Animation ani) {
+		aniRepo.save(ani);
 		return null;
 	}
 	
 	@PutMapping("/{id}")
-	public String updateAni() {
-		
+	public String updateAni(@RequestBody Animation ani, @PathVariable Long id) {
+		aniRepo.save(ani);
 		return null;
 	}
 	
 	@DeleteMapping("/{id}")
-	public String deleteAni() {
-		
+	public String deleteAni(@PathVariable Long id) {
+		aniRepo.deleteById(id);
 		return null;
 	}
 }
